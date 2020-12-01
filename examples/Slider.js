670671b621b6ea7c8a240ff44c55d78f3f6a6870
João Pedro <54006291+Joao010@users.noreply.github.com>
2020-12-01 15:49:32 -0300

    Delete Slider.js

--- examples/Slider.js
+++
@@ -1,8 +0,0 @@
-<Slider
-        minimumValue={0}
-        maximumValue={100}
-        onValueChange={(valorSelecionado) => this.setState({valor: valorSelecionado})}
-        value={this.state.valor}
-        minimumTrackTintColor='green'
-        maximumTrackTintColor='red'
-        />
